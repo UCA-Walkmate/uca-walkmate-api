69a73df8ed2ec746d913bad274c76da16a4cf51d
FR0K3 <00174320@uca.edu.sv>
2024-06-20 13:47:25 -0600

    feat: Created all the CRUD structure for Users

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,23 @@
-export class User {}
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Role } from "src/roles/entities/role.entity";
+
+@Entity('users')
+export class User {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ name: 'name' })
+    name: string;
+
+    @Column({ name: 'email' })
+    email: number;
+
+    @Column({ name: 'password' })
+    password: number;
+
+    // @Column({ name: 'role_id' })
+    @ManyToOne(() => Role, (role) => role.users, { eager: true })
+    role: Role;
+
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,10 +2,11 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly usersService: UsersService) { }
 
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
@@ -13,12 +14,12 @@ export class UsersController {
   }
 
   @Get()
-  findAll() {
+  findAll(): Promise<User[]> {
     return this.usersService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id') id: string): Promise<User> {
     return this.usersService.findOne(+id);
   }
 
