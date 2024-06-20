6383fee21594eba4bcb5ef17980e316642b79bce
FR0K3 <00174320@uca.edu.sv>
2024-06-20 00:06:32 -0600

    feat: Added DB connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    DatabaseModule,    
+  ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
