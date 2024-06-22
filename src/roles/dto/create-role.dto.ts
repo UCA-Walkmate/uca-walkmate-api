525394aab4f40075af88db816d0a44d2599a8eea
FR0K3 <00174320@uca.edu.sv>
2024-06-21 20:04:21 -0600

    feat: Added CRUD for Roles

--- src/roles/dto/create-role.dto.ts
+++ src/roles/dto/create-role.dto.ts
@@ -1,1 +1,9 @@
-export class CreateRoleDto {}
+import { IsNotEmpty, IsString } from "class-validator";
+
+export class CreateRoleDto {
+
+    @IsNotEmpty({ message: 'Name must not be empty' })
+    @IsString({ message: 'Name must be string' })
+    name: string;
+
+}
--- src/roles/roles.service.ts
+++ src/roles/roles.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, InternalServerErrorException, Logger, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateRoleDto } from './dto/create-role.dto';
@@ -7,28 +7,70 @@ import { Role } from './entities/role.entity';
 
 @Injectable()
 export class RolesService {
+  private readonly logger = new Logger('CategoriesService');
+
   constructor(
     @InjectRepository(Role)
     private readonly roleRepository: Repository<Role>,
   ) { }
 
-  create(createRoleDto: CreateRoleDto) {
-    return 'This action adds a new role';
+  async create(createRoleDto: CreateRoleDto) {
+    const name = createRoleDto.name;
+    const exists = await this.roleRepository.findOne({ where: { name } });
+
+    if (exists)
+      throw new ConflictException(`Role with name ${name} already exists`);
+
+    try {
+      const newRole = this.roleRepository.create({ name });
+      await this.roleRepository.save(newRole);
+
+      return newRole;
+    } catch (error) {
+      this.logger.error(error);
+
+      throw new InternalServerErrorException('Unexpected error');
+    }
+
   }
 
   findAll(): Promise<Role[]> {
     return this.roleRepository.find();
   }
 
-  findOne(id: number): Promise<Role> {
-    return this.roleRepository.findOneBy({ id });
+  async findOne(id: number): Promise<Role> {
+    return await this.roleRepository.findOneBy({ id });
   }
 
-  update(id: number, updateRoleDto: UpdateRoleDto) {
-    return `This action updates a #${id} role`;
+  async update(id: number, updateRoleDto: UpdateRoleDto) {
+    const role = await this.roleRepository.preload({
+      id: +id,
+      ...updateRoleDto
+    });
+
+    if (!role)
+      throw new NotFoundException(`Role with id ${id} not found`);
+
+    try {
+      await this.roleRepository.save(role);
+
+      return role;
+    } catch (error) {
+      this.logger.error(error);
+
+      throw new InternalServerErrorException('Unexpected error');
+    }
+
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} role`;
+  async remove(id: number) {
+    const role = await this.roleRepository.findOneBy({ id });
+
+    if (!role)
+      throw new NotFoundException(`Role with id ${id} not found`);
+
+    const result = await this.roleRepository.delete(role);
+
+    return result.affected;
   }
 }
