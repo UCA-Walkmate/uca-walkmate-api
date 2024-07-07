2f5b1fb4d35c39e1918b70c2c125113b762dfc61
FR0K3 <00174320@uca.edu.sv>
2024-07-07 13:56:44 -0600

    feat: Added swagger

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { DeleteResponseInterceptor } from './common/interceptors/delete-response.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,18 @@ async function bootstrap() {
   app.useGlobalInterceptors(
     new DeleteResponseInterceptor(),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('UCA Walkmate API')
+    .setDescription('API that contains all the endpoints for the UCA Walkmate app')
+    .setVersion('1.0')
+    // .addTag('walkmate')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+
+
   await app.listen(3000);
 }
 bootstrap();
